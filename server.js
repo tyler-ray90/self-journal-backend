08006d91471e49694e677745087c2f523f41cd77
Tyler Ray <tyler.p.ray@icloud.com>
2021-04-12 18:12:51 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 // Require our modules
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
+
 
 // Initialize the express App
 const app = express();
 
 
 // Configure settings app.set()
-app.use(logger('dev'));
+// require('dotenv').config();
+require('./config/database');
 
-// Converts incoming Json into req.body
-app.use(express.json());
+// Mount middleware with app.use()
+app.use(logger('dev'));
+app.use(express.json()); // converts incoming Json into req.body
+app.use(cors());
 
 // Mount our routes with app.use()
-app.use('/api/skills', require('./routes/api/journal'));
+app.use('/api/journal', require('./routes/api/journal'));
 
-//Mount middleware with app.use()
-
-
+// Tell app to listen to port 3001
 
 const port = process.env.PORT || 3001
 
